b2be79bbff35643cfcdb827cef1ce262d8a412d5
Chandini Mahalakshmi <104729699+ychandiniml@users.noreply.github.com>
2024-07-24 21:56:56 +0530

    created login and signup components

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Login from '../../client/src/components/login';
+import SignUp from '../../client/src/components/signUp';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+      <SignUp />
+    </div>
+  );
+}
+
+export default App;
